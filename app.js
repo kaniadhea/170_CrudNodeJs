b9b203a51ff225805245a1aec86bf76961589dee
kaniadhea <kaniadhea7@gmail.com>
2024-11-09 13:29:54 +0700

    dbcrud_170

--- app.js
+++ app.js
@@ -1,19 +1,36 @@
 const express = require('express');
 const app = express();
-const todoRoutes = require('./routes/todo.js');
-const port = 3000;
+const todoRoutes = require('./routes/tododb.js');
+const port = process.env.PORT;
+const db = require('./database/db');
+const expressLayouts = require('express-ejs-layouts')
+app.use(expressLayouts);
 
 app.use(express.json());
-app.use('/todos', todoRoutes);
 
+app.use('/todos', todoRoutes);
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.render('index'); // Pastikan ada file index.ejs di dalam folder views
+    res.render('index',{
+        layout : 'layouts/main-layout'
+    }); // Pastikan ada file index.ejs di dalam folder views
 });
 
 app.get('/contact', (req, res) => {
-    res.render('contact'); // Pastikan ada file contact.ejs di dalam folder views
+    res.render('contact', {
+        layout : 'layouts/main-layout'
+    });
+});
+
+app.get('/todo-view', (req, res) => {
+    db.query('SELECT * FROM todos', (err, todos) => {
+        if (err) return res.status(500).send('Internal Server Error');
+        res.render('todo', {
+            layout: 'layouts/main-layout',
+            todos: todos
+        });
+    });
 });
 
 app.listen(port, () => {
